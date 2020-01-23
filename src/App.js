48309cebbe8bd127216bf8f2ca1600b75a17576e
yyang31 <yyang2@mail.csuchico.edu>
2020-01-23 13:39:48 -0800

    add the navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './common/_base.scss';
-import './App.scss';
+import './App.scss'
+
+// navbar import
+import CustomNavbar from './navbar/navbar';
 
 // third party imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomNavbar />
     </div>
   );
 }
