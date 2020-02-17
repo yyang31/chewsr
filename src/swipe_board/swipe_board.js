da2b50f7dacec21a382ec752fd71250f447d1388
Stanley Yang <stanley@wlan-t-226-9-dhcp.CSUChico.EDU>
2020-02-17 08:02:52 -0800

    fix card z-index

--- src/swipe_board/swipe_board.js
+++ src/swipe_board/swipe_board.js
@@ -119,7 +119,7 @@ class SwipeCard extends React.Component {
 
     render() {
         return (
-            <div className="swipe-card">
+            <div className="swipe-card" style={{ zIndex: this.props.index }}>
                 <h1 className="place-name">{this.props.place.name}</h1>
                 {this.props.place.photos ? (
                     <div className="place-photos-cont">
@@ -148,13 +148,16 @@ class SwipeBoard extends React.Component {
     state = {
         radius: 24140.2,    // ~ 15 miles
         nearbyResult: [],
+        indexCount: 1,
     }
 
     updateNearbyResult = (result) => {
         console.log(result);
+        console.log(result.length);
 
         this.setState({
             nearbyResult: result,
+            indexCount: this.state.indexCount + result.length,
         })
     }
 
@@ -163,8 +166,8 @@ class SwipeBoard extends React.Component {
             <div id="board">
                 <NearbySearch radius={this.state.radius} updateNearbyResult={this.updateNearbyResult} />
                 <div id="cards" className={this.state.nearbyResult.length > 0 ? "" : "d-none"}>
-                    {this.state.nearbyResult.map((place) => {
-                        return <SwipeCard key={place.place_id} place={place} />
+                    {this.state.nearbyResult.map((place, index) => {
+                        return <SwipeCard key={place.place_id} place={place} index={this.state.indexCount - this.state.nearbyResult.length + index} />
                     })}
                 </div>
                 <BoardControl display={this.state.nearbyResult.length > 0 ? "" : "d-none"} />
